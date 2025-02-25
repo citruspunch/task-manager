0067579040bb5a2ad2d8f0fc5efd6e99c495f4e6
andrestobar05 <andrewtobar05@gmail.com>
2025-02-25 12:16:50 -0600

    add Bootstrap and react-bootstrap for improved styling; update components to use Bootstrap elements

--- src/components/DeleteTask.jsx
+++ src/components/DeleteTask.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React from "react";
+import { Button } from "react-bootstrap";
 
 export default function DeleteTask({ taskId, onDelete }) {
-    return (
-        <button onClick={() => onDelete(taskId)}>Delete</button>
-    );
-};
+  return (
+    <Button
+      variant="danger"
+      onClick={() => onDelete(taskId)}
+      className="float-right"
+    >
+      Delete
+    </Button>
+  );
+}
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import TaskItem from './TaskItem';
+import { ListGroup } from 'react-bootstrap';
 
 export default function TaskList({ tasks, onDelete }) {
     return (
-        <ul>
+        <ListGroup>
             {tasks.map((task) => (
                 <TaskItem key={task.id} task={task} onDelete={onDelete} />
             ))}
-        </ul>
+        </ListGroup>
     );
 };
