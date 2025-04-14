59229c3b6f3c09c8e57c36b948fcd7af9f0cfe50
andrestobar05 <andrewtobar05@gmail.com>
2025-04-13 18:27:05 -0600

    enhance TaskItem component to support task editing with input field and save functionality

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import DeleteTask from "./DeleteTask";
-import { ListGroup } from "react-bootstrap";
+import { ListGroup, Form } from "react-bootstrap";
+import "../styles/TaskManager.css";
 
-export default function TaskItem({ task, onDelete }) {
+export default function TaskItem({ task, onDelete, onEdit }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [newTaskName, setNewTaskName] = useState(task.name);
+
+  const handleSave = () => {
+    onEdit(task.id, newTaskName);
+    setIsEditing(false);
+  };
+
   return (
     <ListGroup.Item className="d-flex justify-content-between align-items-center">
-      <span>{task.name}</span>
+      {isEditing ? (
+        <input
+        type="text"
+        value={newTaskName}
+        onChange={(e) => setNewTaskName(e.target.value)}
+        onBlur={handleSave}
+        autoFocus
+        className="edit-task-input"
+      />
+      ) : (
+        <span onClick={() => setIsEditing(true)}>{task.name}</span>
+      )}
       <DeleteTask taskId={task.id} onDelete={onDelete} />
     </ListGroup.Item>
   );
