29d386965ea76ad04d165a2fcea34e2302f8c4b7
andrestobar05 <andrewtobar05@gmail.com>
2025-04-13 18:27:00 -0600

    enhance TaskList component to support editing tasks by adding onEdit prop

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import TaskItem from "./TaskItem";
 import { ListGroup } from "react-bootstrap";
 
-export default function TaskList({ tasks, onDelete }) {
+export default function TaskList({ tasks, onDelete, onEdit }) {
   return (
     <ListGroup>
       {tasks.map((task) => (
-        <TaskItem key={task.id} task={task} onDelete={onDelete} />
+        <TaskItem key={task.id} task={task} onDelete={onDelete} onEdit={onEdit}/>
       ))}
     </ListGroup>
   );
